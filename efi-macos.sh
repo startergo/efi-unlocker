e5aceb0022edc4b10ec8b172796f24856473f254
startergo <7897244+startergo@users.noreply.github.com>
2024-06-09 10:15:25 -0400

    Update efi-macos.sh

--- efi-macos.sh
+++ efi-macos.sh
@@ -33,8 +33,8 @@ printf "\nPatching 64-bit ROM...\n"
 cp -v /Applications/VMware\ Fusion.app/Contents/Library/roms/x86_x64/EFI64.ROM .
 ./macos/UEFIPatch EFI64.ROM patches.txt -o EFI64-MACOS.ROM
 rm -fv EFI64.ROM
-# Check version is 17+
-if [[ ${product[0]} -ge 17 ]]; then
+# Check version is 13+
+if [[ ${product[0]} -ge 13 ]]; then
    printf "Patching 32-bit TPM 2.0 ROM...\n"
    cp -v /Applications/VMware\ Fusion.app/Contents/Library/roms/x86_x64/EFI20-32.ROM .
    ./macos/UEFIPatch EFI20-32.ROM patches.txt -o EFI20-32-MACOS.ROM
